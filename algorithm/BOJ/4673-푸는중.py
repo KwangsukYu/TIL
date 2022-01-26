fe456268fb402a772726eb1fc56a711e0656917c
KwangsukYu <soye0822@gmail.com>
2022-01-27 00:12:44 +0900

    BOJ 220127

--- algorithm/BOJ/4673-푸는중.py
+++ algorithm/BOJ/4673-푸는중.py
@@ -1,17 +1,15 @@
 # 정수 n이면 n과 n의 각 자리수 더하기 d(75) = 75 + 7 + 5
 num_list = []
-for i in range(127, 130):
-    c = i//10000
-    a = i//1000
-    b = i//100
-    x = i//10
-    y = i % 10
-    i = i + x + y + a + b + c
-    num_list.append(i)
 
-print(num_list)
-# for j in range(1, 150):
-#     if j not in num_list:
-#         print(j)
+def lenth_num(num):
+    self_n = num
+    for i in str(num):
+        self_n += int(i)
+    return self_n
 
-127 + 1 + 27 + 7 
+for i in range(1, 10000):
+    num_list.append(lenth_num(i))
+
+for j in range(1, 10000):
+    if j not in num_list:
+        print(j)
