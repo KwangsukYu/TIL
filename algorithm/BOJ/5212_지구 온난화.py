d5f29903da9dfc787fd49379e10ca8a584d26937
KwangsukYu <soye0822@gmail.com>
2022-04-26 23:26:24 +0900

    BOJ 220426

--- algorithm/BOJ/5212_지구 온난화.py
+++ algorithm/BOJ/5212_지구 온난화.py
@@ -1,5 +1,8 @@
+# 인풋을 받고 배열 외곽에 바다를 추가해줌, 계산하기 편함
 N, M = map(int, input().split())
 arr = [['.'] * (M+2)] +  [['.'] + list(input()) + ['.'] for _ in range(N)] + [['.'] * (M+2)]
+
+# 다음번에 바다가 될 섬의 좌표를 찾아서 sea에 넣어줌
 sea = []
 for i in range(N+2):
     for j in range(M+2):
@@ -8,14 +11,18 @@ for i in range(N+2):
             for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
                 ni = i + di
                 nj = j + dj
+
+                # 한쪽 면이 바다면 cnt 증가시키고 3이상이면 바다
                 if arr[ni][nj] == '.':
                     cnt += 1
                 if cnt >= 3:
                     sea.append((i, j))
 
+# 50년 뒤 바다가 될 섬들을 바다로 만들기
 for r, c in sea:
     arr[r][c] = '.'
 
+# 지도를 최소로 줄여야하므로 위아래 바다만 있는 경우를 제외
 i = 0
 while True:
     if arr[i].count('X') == 0:
@@ -31,8 +38,12 @@ while True:
     else:
         break
 
+# 위 아래 최소값은 윗 부분에서 완성 됨
+# 좌 우의 최소값을 구하는 과정
 minj = 11
 maxj = 0
+
+# 섬이 등장하는 x좌표의 최소값과 최대값을 구하고
 for i in range(len(arr)):
     for j in range(M+2):
         if arr[i][j] == 'X':
@@ -41,6 +52,7 @@ for i in range(len(arr)):
             if j > maxj:
                 maxj = j
 
+# 인덱스 슬라이싱으로 출력하기
 if arr:
     for i in arr:
         print(''.join(i[minj:maxj+1]))
--- algorithm/BOJ/5547_일루미네이션.py
+++ algorithm/BOJ/5547_일루미네이션.py
@@ -1,12 +1,18 @@
 from collections import deque
+from pprint import pprint
 
+# A형 문제랑은 다르게 r,c 에서 r 값의 홀짝에 따라 변함
+# 맞춰서 짝수일 경우 홀수일 경우로 나눔
 even_d = ((-1, -1), (-1, 0), (0, -1), (0, 1), (1, -1), (1, 0))
 odd_d = ((-1, 0), (-1, 1), (0, -1), (0, 1), (1, 0), (1, 1))
 
+# 인풋을 받는데 0으로 전체 배열을 한 번 더 감싸줌 (== BFS 용)
 M, N = map(int, input().split())
 
 arr = [[0] * (M+2)] + [[0] + list(map(int, input().split())) + [0] for _ in range(N)] + [[0] * (M+2)]
 
+# BFS로 바깥의 0부터 시작해서 갈 수 있는 모든 경로를 탐색해서 -1로 변환
+# == 건물 밖은 모두 -1
 q = deque()
 q.append((0, 0))
 arr[0][0] = -1
@@ -18,6 +24,7 @@ while q:
             nr = r + dr
             nc = c + dc
 
+            # 인덱스 상하좌우 0을 추가했으니 범위도 +2 씩
             if 0 <= nr < N+2 and 0 <= nc < M+2 and arr[nr][nc] == 0:
                 arr[nr][nc] = -1
                 q.append((nr, nc))
@@ -29,30 +36,45 @@ while q:
             if 0 <= nr < N+2 and 0 <= nc < M+2 and arr[nr][nc] == 0:
                 arr[nr][nc] = -1
                 q.append((nr, nc))
-cnt = 0
 
+# 위의 BFS가 완료되면 바깥 부분은 -1, 벽은 1, 안쪽은 0인 상태
+# pprint(arr)
+
+# 이제 벽 쪽 외곽을 탐색하는 BFS
+# 정답용 cnt와 visited를 인덱스에 맞추어 생성
+cnt = 0
 visited = [[0] * (M+2) for _ in range(N+2)]
+
+# 각각 순회하면서
 for i in range(N+2):
     for j in range(M+2):
+        
+        # 벽을 찾는 과정
+        # 벽인데 아직 방문하지 않았을 경우, 방문 체크하고 BFS시작
         if arr[i][j] == 1 and visited[i][j] == 0:
             q = deque()
             q.append((i, j))
             visited[i][j] = 1
+
             while q:
                 r, c = q.popleft()
 
+                # r이 홀 수 일때 짝수 범위를 돌면서
                 if r % 2:
                     for dr, dc in odd_d:
                         nr = r + dr
                         nc = c + dc
 
+                        # 만약 인덱스 범위안에 이어진 벽이있고 방문하지 않았으면 방문체크하고 q에 추가
                         if 0 <= nr < N+2 and 0 <= nc < M+2 and arr[nr][nc] == 1 and visited[nr][nc] == 0:
                             visited[nr][nc] = 1
                             q.append((nr, nc))
                         
-                        elif visited[nr][nc] == 0 and arr[nr][nc] == -1:
+                        # 외곽 면인 경우는 -1과 맞닿아있으므로 -1일 경우 카운팅
+                        elif arr[nr][nc] == -1:
                             cnt += 1
                 
+                # 짝 수 일때 위와 동일
                 else:
                     for dr, dc in even_d:
                         nr = r + dr
@@ -66,4 +88,5 @@ for i in range(N+2):
                             cnt += 1
                 
 
+# 결과 출력
 print(cnt)
