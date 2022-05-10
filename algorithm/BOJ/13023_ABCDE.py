793a4a986d46d41e7bbc6fde78a10f63fdea0edc
KwangsukYu <soye0822@gmail.com>
2022-05-10 22:09:34 +0900

    BOJ 220510

--- algorithm/BOJ/13023_ABCDE.py
+++ algorithm/BOJ/13023_ABCDE.py
@@ -1,26 +1,30 @@
 def DFS(s, cnt):
-    global ans
+
+    # 만약 depth가 4면 종료
     if cnt == 4:
-        ans = True
         print(1)
         exit()
 
+    # 아니면 추가하고 삭제하는 식으로
     for j in lst[s]:
         if not visited[j]:
             visited[j] += 1
             DFS(j, cnt+1)
             visited[j] = 0
 
+# A - B - C - D - E depth가 4번이면 바로 리턴해주는 형식
 N, M = map(int, input().split())
-ans = False
+# ans = False
 lst = [[] for _ in range(N)]
 
 
+# 친구관계는 양방향이므로 둘다 받아줌
 for i in range(M):
     x, y = map(int, input().split())
     lst[x].append(y)
     lst[y].append(x)
 
+# 각각 DFS를 돌려서 depth가 4가 되는지 확인
 for i in range(N):
     visited = [0] * N
     visited[i] += 1
--- algorithm/BOJ/1600_말이 되고픈 원숭이.py
+++ algorithm/BOJ/1600_말이 되고픈 원숭이.py
@@ -2,17 +2,19 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-
-def BFS(sr, sc, cnt, d):
+# 좌표, 동작 횟수, 말 움직임 횟수
+def BFS(sr, sc, cnt, j):
     q = deque()
-    q.append((sr, sc, cnt, d))
+    q.append((sr, sc, cnt, j))
 
     while q:
         r, c, cnt, jump = q.popleft()
 
+        # 도착지면 동작 횟수를 리턴해줌
         if r == N - 1 and c == M - 1:
             return cnt
 
+        # 일단 상하좌우로는 무조건 움직일 수 있음
         for dr, dc in monkey:
             nr = r + dr
             nc = c + dc
@@ -21,11 +23,13 @@ def BFS(sr, sc, cnt, d):
                 visited[nr][nc][jump] = 1
                 q.append((nr, nc, cnt+1, jump))
 
+        # 아직 말 처럼 움직일 수 있는 횟수가 남았다면...
         if jump < K:
             for dr, dc in horse:
                 nr = r + dr
                 nc = c + dc
 
+                # 횟수 1 증가시키고 q에 넣어줌
                 if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == '0' and visited[nr][nc][jump+1] == 0:
                     visited[nr][nc][jump+1] = 1
                     q.append((nr, nc, cnt+1, jump+1))
@@ -35,11 +39,17 @@ def BFS(sr, sc, cnt, d):
 K = int(input())
 M, N = map(int, input().split())
 
+# 말과 원숭이의 이동 패턴
 horse = [(-2, -1), (-1, -2), (1, -2), (2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1)]
 monkey = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
+# 3차원 배열 설정 [r][c][jump]
+# jump는 말처럼 움직인 횟수
 arr = [list(input().split()) for _ in range(N)]
 visited = [[[0] * (K+1) for _ in range(M)] for _ in range(N)]
+
+
+# 시작 지점 설정하고 BFS 시작!
 visited[0][0][0] = 1
 ans = BFS(0, 0, 0, 0)
 print(ans)
