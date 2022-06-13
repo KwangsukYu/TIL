f47cd13ef6177e4c13f288a93ae7da5bddd8ae14
KwangsukYu <soye0822@gmail.com>
2022-06-13 21:13:30 +0900

    BOJ 220613

---
+++ algorithm/BOJ/5585_거스름돈.py
@@ -0,0 +1,15 @@
+N = int(input())
+change = 1000 - N
+
+money = [500, 100, 50, 10, 5, 1]
+print(change)
+cnt = 0
+for i in money:
+    while True:
+        if change - i >= 0:
+            cnt += 1
+            change -= i
+        else:
+            break
+
+print(cnt)
